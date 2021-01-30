8dd4214c5e9a4d94051fef600405a1f9b51d2b0b
Sam Hirsch <sam.hirsch66@gmail.com>
2021-01-29 19:34:36 -0700

    fixed ssr

--- src/components/global.js
+++ src/components/global.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-global.window = {};
+global.isServer = typeof window === 'undefined';
 export const Container = styled.div`
   max-width: 1200px;
   width: 100%;
