f8764b6b2f3ea99b8d46bf247a8c3475b7e445da
Sam Hirsch <sam.hirsch66@gmail.com>
2021-03-18 09:05:06 -0600

    updated redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,13 +2,19 @@ exports.createPages = async function({ actions, graphql }) {
   actions.createRedirect({
     fromPath: '/privacy-policy',
     toPath: '/privacy-policy.pdf',
-    statusCode: 200,
+    statusCode: 301,
     force: true,
   });
   actions.createRedirect({
     fromPath: '/terms-of-use',
     toPath: '/terms-of-use.pdf',
-    statusCode: 200,
+    statusCode: 301,
+    force: true,
+  });
+  actions.createRedirect({
+    fromPath: '/refer/*',
+    toPath: '/',
+    statusCode: 302,
     force: true,
   });
 };
